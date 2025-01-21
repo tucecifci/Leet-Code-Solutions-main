c4f8ae014738ab7d9ae6ebaf2f235d67a3c21477
tucecifci <tucecifcii@gmail.com>
2025-01-21 11:09:10 +0300

    info added

--- 169.Majority-element.js
+++ 169.Majority-element.js
@@ -1,4 +1,4 @@
 var majorityElement = function(nums) {
     nums.sort((a,b) => a-b);
-    return nums[Math.floor(nums.length / 2)];
+    return nums[Math.floor(nums.length / 2)]; //Çoğunluk elemanı dizide  n/2'den fazla yer kapladığından, sıralandığında dizinin ortasına denk gelir.
 };
